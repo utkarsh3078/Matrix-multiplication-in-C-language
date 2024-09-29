8c030c9b0de27669dc0779eb49b1c76f79f0b23e
Utkarsh Yadav <utkarshyadav3078@gmail.com>
2024-09-29 15:50:43 +0530

    Add files via upload

---
+++ matrix_multiplication.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+int main() {
+    int rows1, cols1, rows2, cols2;
+
+    printf("Enter the number of rows and columns for matrix 1 respectively: ");
+    scanf("%d %d", &rows1, &cols1);
+
+    printf("Enter the number of rows and columns for matrix 2 respectively: ");
+    scanf("%d %d", &rows2, &cols2);
+
+    // Check if matrix multiplication is possible
+    if (cols1 != rows2) {
+        printf("Matrix multiplication is not possible.\n");
+        return 1;
+    }
+
+    int matrix1[rows1][cols1], matrix2[rows2][cols2], result[rows1][cols2];
+
+    // Take input for matrix 1
+    printf("Enter elements for matrix 1:\n");
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols1; j++) {
+            scanf("%d", &matrix1[i][j]);
+        }
+    }
+
+    // Take input for matrix 2
+    printf("Enter elements for matrix 2:\n");
+    for (int i = 0; i < rows2; i++) {
+        for (int j = 0; j < cols2; j++) {
+            scanf("%d", &matrix2[i][j]);
+        }
+    }
+
+    // Perform matrix multiplication
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols2; j++) {
+            result[i][j] = 0;
+            for (int k = 0; k < cols1; k++) {
+                result[i][j] += matrix1[i][k] * matrix2[k][j];
+            }
+        }
+    }
+
+    // Print the resulting matrix
+    printf("Resultant matrix:\n");
+    for (int i = 0; i < rows1; i++) {
+        for (int j = 0; j < cols2; j++) {
+            printf("%d ", result[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
